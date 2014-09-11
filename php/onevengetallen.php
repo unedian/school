d2464145854dc9f09d4af6184e0add76a96abaf5
unedian <goldend4gon@otmail.com>
2014-09-11 14:27:27 +0200

    opdracht 3

--- php/onevengetallen.php
+++ php/onevengetallen.php
@@ -1,4 +1,4 @@
-<h1> Milo heeft tering veel swag </h1>
+
 <?php	
 
 $getal = $_GET['getal'];
@@ -8,10 +8,4 @@ if ($getal % 2 == 0) {
 	echo $getal . " is oneven";
 }
 
-<<<<<<< HEAD
-
 ?>
-=======
-oneven();
-?>
->>>>>>> origin/master
